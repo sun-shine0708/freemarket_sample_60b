48deae6157b2eab3ee9d6a5055773e18901162ea
sun-shine0708 <one.rockys@gmail.com>
2019-10-07 18:43:09 +0900

    front-function

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root 'products#index'
   resources :products, only: [:index]
-  resources :users
+  resources :users do
+    member do
+      get 'preview'
+    end
+  end
 end
