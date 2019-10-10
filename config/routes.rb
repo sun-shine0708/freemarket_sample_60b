be35ea22e1da05040ce90e3dc7b2ec54733ad5f7
Nishi-Shin <se031247@gmail.com>
2019-10-10 18:57:34 +0900

    new_sells_view_create

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'products#index'
-  resources :products, only: [:index]
+  resources :products, only: [:index, :new]
   resources :users do
     member do
       get 'preview'
