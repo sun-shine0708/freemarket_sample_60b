1294e6012bd59a89af6f0dfdd6fff350b65b60a8
nakaimomoko <mommkoman.sanjo.0318@icloud.com>
2019-10-10 13:50:49 +0900

    edit DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,25 +10,59 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191007040539) do
+ActiveRecord::Schema.define(version: 20191009103508) do
+
+  create_table "creditcards", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "card_number",      null: false
+    t.integer  "expiration_month", null: false
+    t.integer  "expiration_year",  null: false
+    t.integer  "security_code",    null: false
+    t.integer  "user_id",          null: false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.index ["user_id"], name: "index_creditcards_on_user_id", using: :btree
+  end
+
+  create_table "streetaddresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "first_name",      null: false
+    t.string   "last_name",       null: false
+    t.string   "first_name_kana", null: false
+    t.string   "last_name_kana",  null: false
+    t.integer  "post_number",     null: false
+    t.string   "prefectures",     null: false
+    t.string   "city",            null: false
+    t.string   "address",         null: false
+    t.string   "building_name"
+    t.integer  "phone_number"
+    t.integer  "user_id",         null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.index ["user_id"], name: "index_streetaddresses_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name",                                              null: false
-    t.string   "name_kana",                                         null: false
+    t.string   "first_name",                                        null: false
+    t.string   "first_name_kana",                                   null: false
     t.string   "nickname",                                          null: false
     t.string   "email",                                default: "", null: false
     t.string   "encrypted_password",                   default: "", null: false
-    t.integer  "birthday",                                          null: false
+    t.integer  "birth_year",                                        null: false
     t.text     "comment",                limit: 65535
     t.text     "image",                  limit: 65535
-    t.string   "payment"
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at",                                        null: false
     t.datetime "updated_at",                                        null: false
+    t.integer  "phone_number",                                      null: false
+    t.string   "last_name",                                         null: false
+    t.string   "last_name_kana",                                    null: false
+    t.integer  "birth_month",                                       null: false
+    t.integer  "birth_day",                                         null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "creditcards", "users"
+  add_foreign_key "streetaddresses", "users"
 end
