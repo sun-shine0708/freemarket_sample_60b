2ac9b7d47e85d53b1cdc51118d363fbb513d1b5f
sun-shine0708 <one.rockys@gmail.com>
2019-10-14 20:07:13 +0900

    edit-delete-create

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,29 @@
 class ProductsController < ApplicationController
 
-  
   def index
   end
 
   def new
     @products = Product.new
     @products.images.build
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    product = Product.find(params[:id])
+    if product.user_id == current_user.id
+      product.update(tweet_params)
+    end
+  end
+
+  def destroy
+    product = Product.find(params[:id])
+    if product.user_id == current_user.id
+      product.destroy
+    end
   end
 
   def show
@@ -16,7 +33,7 @@ class ProductsController < ApplicationController
 
   def buy_confirmation
     @products = Product.new
-  end  
+  end
 
   def create
     @products = Product.new(product_params)
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'products#index'
-  resources :products, only: [:index, :new, :show, :create, :destroy, :update] do
+  resources :products do
     member do
       get 'buy_confirmation'
     end
