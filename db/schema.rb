4371206027c30b52f483939f662a40779df20132
daichikvn <daichikvn7000@gmail.com>
2019-10-15 04:31:35 +0900

    Update.schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191013133500) do
+ActiveRecord::Schema.define(version: 20191014051841) do
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
