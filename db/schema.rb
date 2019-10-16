5fe71a80737747fa49bd865ed78b450f9cc1a9da
nakaimomoko <mommkoman.sanjo.0318@icloud.com>
2019-10-16 21:31:01 +0900

    view-selectbox-key

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191014051841) do
+ActiveRecord::Schema.define(version: 20191016105606) do
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
@@ -80,7 +80,7 @@ ActiveRecord::Schema.define(version: 20191014051841) do
     t.string   "last_name_kana",  null: false
     t.string   "first_name_kana", null: false
     t.string   "post_number",     null: false
-    t.string   "prefectures",     null: false
+    t.integer  "prefecture",      null: false
     t.string   "city",            null: false
     t.string   "address",         null: false
     t.string   "building_name"
