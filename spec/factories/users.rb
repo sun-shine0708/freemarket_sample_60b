79ffca24f3be17da39bda2cf724a3a8ffcd46734
nakaimomoko <mommkoman.sanjo.0318@icloud.com>
2019-10-19 01:20:27 +0900

    api-test

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,14 +2,18 @@ FactoryBot.define do
 
   factory :user do
     password = Faker::Internet.password(7)
-    name                  { Faker::Name.name }
-    name_kana             {"ヤマダタロウ"}
-    nickname              {"toro"}
-    email                 { Faker::Internet.free_email }
-    password              { password }
-    password_confirmation { password }
-    birthday              {"1006"}
-    comment               {"aaaaaaa"}
+    last_name                  { Faker::Name.name }
+    first_name                 { Faker::Name.name }
+    first_name_kana            {"ヤマダ"}
+    last_name_kana             {"タロウ"}
+    nickname                   {"toro"}
+    email                      { Faker::Internet.free_email }
+    password                   { password }
+    password_confirmation      { password }
+    birth_year                 {"1999"}
+    birth_month                {"12"}
+    birth_day                  {"19"}
+    phone_number               {"09000000000"}
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,18 +8,32 @@ describe User do
       expect(user).to be_valid
     end
 
-    # nameが空では登録できないこと
-    it "is invalid without a name" do
-      user = build(:user, name: nil)
+    # last_nameが空では登録できないこと
+    it "is invalid without a last_name" do
+      user = build(:user, last_name: nil)
       user.valid?
-      expect(user.errors[:name]).to include("can't be blank")
+      expect(user.errors[:last_name]).to include("can't be blank")
     end
 
-    # name_kanaが空では登録できないこと
-    it "is invalid without a name_kana" do
-      user = build(:user, name_kana: nil)
+    # first_nameが空では登録できないこと
+    it "is invalid without a first_name" do
+      user = build(:user, first_name: nil)
       user.valid?
-      expect(user.errors[:name_kana]).to include("can't be blank")
+      expect(user.errors[:first_name]).to include("can't be blank")
+    end
+
+    # first_name_kanaが空では登録できないこと
+    it "is invalid without a first_name_kana" do
+      user = build(:user, first_name_kana: nil)
+      user.valid?
+      expect(user.errors[:first_name_kana]).to include("can't be blank")
+    end
+
+    # last_name_kanaが空では登録できないこと
+    it "is invalid without a last_name_kana" do
+      user = build(:user, last_name_kana: nil)
+      user.valid?
+      expect(user.errors[:last_name_kana]).to include("can't be blank")
     end
 
     # nicknameが空では登録できないこと
@@ -50,18 +64,32 @@ describe User do
       expect(user.errors[:password_confirmation]).to include("doesn't match Password")
     end
 
-    # birthdayが空では登録できないこと
-    it "is invalid without a birthday" do
-      user = build(:user, birthday: nil)
+    # birth_yearが空では登録できないこと
+    it "is invalid without a birth_year" do
+      user = build(:user, birth_year: nil)
       user.valid?
-      expect(user.errors[:birthday]).to include("can't be blank")
+      expect(user.errors[:birth_year]).to include("can't be blank")
     end
 
-    # commentが空では登録できないこと
-    it "is invalid without a comment" do
-      user = build(:user, comment: nil)
+    # birth_monthが空では登録できないこと
+    it "is invalid without a birth_month" do
+      user = build(:user, birth_month: nil)
       user.valid?
-      expect(user.errors[:comment]).to include("can't be blank")
+      expect(user.errors[:birth_month]).to include("can't be blank")
+    end
+
+    # birth_dayが空では登録できないこと
+    it "is invalid without a birth_day" do
+      user = build(:user, birth_day: nil)
+      user.valid?
+      expect(user.errors[:birth_day]).to include("can't be blank")
+    end
+
+     # phone_numberが空では登録できないこと
+     it "is invalid without a phone_number" do
+      user = build(:user, phone_number: nil)
+      user.valid?
+      expect(user.errors[:phone_number]).to include("can't be blank")
     end
 
     # nicknameが20文字以上であれば登録できないこと
