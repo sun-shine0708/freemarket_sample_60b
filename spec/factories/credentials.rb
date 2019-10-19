afd4a50c22359958bb77bb731eef0d9f96468baf
daichikvn <daichikvn7000@gmail.com>
2019-10-19 18:21:44 +0900

    rspec-edit

--- spec/factories/credentials.rb
+++ spec/factories/credentials.rb
@@ -1,14 +1,14 @@
 FactoryBot.define do
-
+  #facebook
   factory :credential do
     uid  { Faker::Omniauth.facebook[:uid] }
     provider  { Faker::Omniauth.facebook[:provider] }
   end
 
-  factory :credential do
-    uid  { Faker::Omniauth.google[:uid] }
-    provider  { Faker::Omniauth.google[:provider] }
-  end
-
+  #google
+  # factory :credential do
+  #   uid  { Faker::Omniauth.google[:uid] }
+  #   provider  { Faker::Omniauth.google[:provider] }
+  # end
 end
 
