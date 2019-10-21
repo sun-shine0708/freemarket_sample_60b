40bf89867b5aa491163472f521ca7cc72a1d8049
xJoDenx <weider.in.jelly.61@gmail.com>
2019-10-21 16:08:58 +0900

    edit_products/edit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -15,7 +15,6 @@ before_action :set_parent_category, only: [:new, :create, :edit]
         break
       end
     end
- 
   end
 
   def new
@@ -23,16 +22,24 @@ before_action :set_parent_category, only: [:new, :create, :edit]
     @products.images.build
   end
 
-  def get_category_children
-    @category_children = Category.find_by(name: "#{params[:parent_name]}", ancestry: nil).children
-  end
-
-  def get_category_grandchildren
-    @category_grandchildren = Category.find("#{params[:child_id]}").children
-  end
-
   def edit
     @product = Product.find(params[:id])
+
+    # @category_parent_array = [{name:'---', id:'---'}]
+    # Category.roots.each do |parent|
+    #   @parent = {name: parent.name, id: parent.id}
+    #   @category_parent_array << @parent
+    # end
+    @category_children_array = [{name:'---', id:'---'}]
+    (@product.category.root.children).each do |child|
+      @children = {name: child.name, id: child.id}
+      @category_children_array << @children
+    end
+    @category_grandchildren_array = [{name:'---', id:'---'}]
+    (@product.category.parent.children).each do |grandchild|
+      @grandchildren = {name:grandchild.name, id:grandchild.id}
+      @category_grandchildren_array << @grandchildren
+    end
   end
 
   def update
@@ -63,7 +70,7 @@ before_action :set_parent_category, only: [:new, :create, :edit]
 
 
   def buy_confirmation
-    @products = Product.new
+    @product = Product.new
   end
 
   def create
@@ -75,20 +82,26 @@ before_action :set_parent_category, only: [:new, :create, :edit]
     end
   end
 
+  def get_category_children
+    @category_children = Category.find_by(id: "#{params[:parent_id]}", ancestry: nil).children
+  end
+
+  def get_category_grandchildren
+    @category_grandchildren = Category.find("#{params[:child_id]}").children
+  end
+
+
   private
   def  product_params
     params.require(:product).permit(:name, :comment, :price, :status, :costcharge, :delivery_way, :delivery_area, :delivery_date, :category_id, :root_category, images_attributes: [:url]).merge(seller_id: current_user.id)
   end
 
   def set_parent_category
-    @category_parent_array = []
-    Category.where(ancestry: nil).each do |parent|
-      @category_parent_array << parent.name
+    @category_parent_array = [{name:'---', id:'---'}]
+    Category.roots.each do |parent|
+      @parent = {name: parent.name, id: parent.id}
+      @category_parent_array << @parent
     end
   end
-
-  # def set_user
-  #   @user = User.find(current_user)
-  # end
 end
 
