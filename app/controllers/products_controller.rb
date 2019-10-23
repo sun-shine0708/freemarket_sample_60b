e0d9e6fa675d7e1ecc97aab6ca5cb643d5839d3f
sun-shine0708 <one.rockys@gmail.com>
2019-10-23 19:31:36 +0900

    edit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -98,10 +98,11 @@ class ProductsController < ApplicationController
 
   def search
     # 検索オブジェクト作成
-    @search = Product.includes(:category).where(category_id: Category.find(params:id).root.subtree).ransack(params[:q])
-    # @search = Product.includes(:category).ransack(params[:q])
+    # @search = Product.includes(:category).where(category_id: Category.find(params:id).root.subtree).ransack(params[:q])
+    @search = Product.includes(:category).ransack(params[:q])
     # 検索結果表示
     @products = @search.result(distinct: true)
+    @namesearchs = Product.where('name LIKE(?)', "%#{params[:keyword]}%").limit(24)
   end
 
   def create
