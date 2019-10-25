c28e26d4977f2cbaafa4d3e8ed50e3592d632e78
nakaimomoko <mommkoman.sanjo.0318@icloud.com>
2019-10-25 20:09:56 +0900

    turbolinks-edit2

--- app/assets/javascripts/products.show.js
+++ app/assets/javascripts/products.show.js
@@ -1,4 +1,4 @@
-$(document).on('turbolinks:load', function() {
+$(function() {
   $(function() {
     $('img.details__photo__under__image').click(function(){
       var ImgSrc = $(this).attr("src");
